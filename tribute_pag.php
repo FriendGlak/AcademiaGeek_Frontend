1946be476dc021b5adff29a10f9f6289fab28b9b
FriendGlak <camilojheans@gmail.com>
2021-06-03 01:19:24 -0500

    Añadida la estructura de linea de tiempo

--- tribute_pag.php
+++ tribute_pag.php
@@ -1,4 +1,5 @@
-<?php $title = "Tributo"; require 'views/head.php' ?>
+<?php $title = "Tributo";
+require 'views/head.php' ?>
 <?php require 'views/navbar.php' ?>
 <main id="main" class="container-fluid bg-secondary">
     <div class="container p-1">
@@ -11,52 +12,96 @@
             </figcaption></em>
         </figure>
         <section class="text-center" id="tribute-info">
-            <h3 id="headline" class="text-white text-center">Esta es la linea de tiempo dentro de la vida del
-                Dr.Nikola Tesla</h3>
-            <ul class="text-white text-center">
-                <li><strong>1856</strong> - Nació en Smiljan (Gospić, Imperio austrohúngaro) Ver y modificar los
-                    datos
-                    en Wikidata</li>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="card">
+                            <div class="card-body --bs-teal border-bottom-0">
+                                <h5 class="card-title text-white">Esta es la linea de tiempo dentro de la vida del Dr.Nikola Tesla</h5>
+                                <div id="content ">
+                                    <ul class="linea_tiempo">
+                                        <li class="event text-white-50" data-date="12:30 - 1:00pm">
+                                            <h3 class="text-white">Registration</h3>
+                                            <p>Get here on time, it's first come first serve. Be late, get turned away.</p>
+                                        </li>
+                                        <li class="event text-white-50" data-date="2:30 - 4:00pm">
+                                            <h3 class="text-white">Opening Ceremony</h3>
+                                            <p>Get ready for an exciting event, this will kick off in amazing fashion with MOP &amp; Busta Rhymes as an opening show.</p>
+                                        </li>
+                                        <li class="event text-white-50" data-date="5:00 - 8:00pm">
+                                            <h3 class="text-white">Main Event</h3>
+                                            <p>This is where it all goes down. You will compete head to head with your friends and rivals. Get ready!</p>
+                                        </li>
+                                        <li class="event text-white-50" data-date="8:30 - 9:30pm">
+                                            <h3 class="text-white">Closing Ceremony</h3>
+                                            <p>See how is the victor and who are the losers. The big stage is where the winners bask in their own glory.</p>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
+            <!--Cartas-->
+            <!-- <div class="row row-cols-1 row-cols-md-5 g-4">
+                <div class="col">
+                    <div class="card">
+                        <img src="./Utilitys/tower_2.png" class="card-img-top imgcard1" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card ">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        </div>
+                    </div>
+                </div>
+            </div> -->
+
+            <!-- <ul class="text-white text-center">
+                <li><strong>1856</strong> - Nació en Smiljan (Gospić, Imperio austrohúngaro) Ver y modificar los
+                    datos en Wikidata
+                </li>
             </ul>
             <blockquote class="text-white text-center" cite="https://www.goodreads.com/author/quotes/278.Nikola_Tesla">
                 <p>
                     “Si quieres encontrar los secretos del universo,
                     piensa en términos de energía, frecuencia y vibración”
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci unde commodi distinctio
-                    ratione! Dolor, molestiae inventore et sed voluptates, possimus quae, nulla quibusdam
-                    reprehenderit ducimus labore eaque ut dignissimos! Optio, recusandae eligendi facilis cumque at
-                    suscipit dolorem reiciendis, perspiciatis voluptates quisquam vel odit, eveniet modi ducimus
-                    vero ad. Quibusdam, aliquam iste. Earum voluptatem, incidunt animi possimus dicta neque
-                    accusamus culpa, tempore magnam dolorum voluptas fugit nam voluptates harum doloremque rerum
-                    eaque saepe enim perferendis, laborum sit illo aliquam. Explicabo suscipit quis repellendus.
-                    Cupiditate eveniet dicta possimus dolores sint reprehenderit consequuntur quam, omnis similique,
-                    ipsum nam culpa accusamus itaque. Repellat molestias iusto non quisquam quibusdam aliquid eos.
-                    Quam ducimus voluptatibus, illo dolorem consectetur qui, nostrum error tempora blanditiis
-                    consequatur officiis eum eaque! Asperiores temporibus laborum maxime quam autem eius tempora
-                    dolorem error, exercitationem, accusantium eos veniam labore dicta harum! Quidem,
-                    necessitatibus, magni eligendi sed nemo nobis ut numquam voluptatem consectetur excepturi
-                    officia neque nam eveniet laboriosam velit ipsum accusamus quae commodi, id beatae incidunt
-                    atque inventore fugiat reprehenderit! Quo a hic fugit! Adipisci unde impedit a aliquam. A vitae
-                    reiciendis dicta sint similique voluptatem illo eius dignissimos aut officia ad numquam iste
-                    repellat, at voluptatibus obcaecati corrupti error eum aperiam! Unde asperiores impedit officiis
-                    odio placeat aut repudiandae, quod facere inventore quasi? Voluptas doloribus laboriosam
-                    perferendis sit minus aliquid temporibus beatae nam, odit vel debitis, consectetur, libero ab
-                    iusto qui quam! Accusamus autem obcaecati accusantium incidunt sunt illo. Hic eius velit quasi.
-                    Pariatur optio consequatur officiis necessitatibus ducimus nesciunt dolor provident? Quaerat
-                    accusantium quae nemo suscipit illum inventore vel iure ut id. Laborum at incidunt odit
-                    doloribus expedita quod veritatis corporis amet error? Assumenda officia alias nam sit tenetur
-                    quibusdam similique itaque aut, odit in ipsa nostrum eveniet commodi, possimus omnis dolor
-                    nesciunt? Laboriosam enim culpa dolorum, delectus perspiciatis aut tempore.
                 </p>
                 <cite>― Nikola Tesla</cite>
-            </blockquote>
-            <h3 class="text-white">
+            </blockquote> -->
+            <!-- <h3 class="text-white m-1">
                 Si tienes tiempo, podrías leer más acerca de este increíble inventor en
                 <button type="button" class="btn btn-secondary" data-bs-toggle="tooltip" data-bs-placement="top" title="Tooltip on top">
                     Wikipedia
-                </button>
-                <!-- <a id="tribute-link" class="text-white text-center text-decoration-none"
+                </button> -->
+            <!-- <a id="tribute-link" class="text-white text-center text-decoration-none"
                         href="https://es.wikipedia.org/wiki/Nikola_Tesla" target="_blank">Wikipedia
                         entry</a>. -->
             </h3>
