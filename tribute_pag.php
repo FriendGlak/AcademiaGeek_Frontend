020e2a68e6fcf9f5e331b3cca61b31c8efd003b1
FriendGlak <camilojheans@gmail.com>
2021-06-03 02:25:59 -0500

    Linea de tiempo añadida

--- tribute_pag.php
+++ tribute_pag.php
@@ -18,23 +18,46 @@ require 'views/head.php' ?>
                         <div class="card">
                             <div class="card-body --bs-teal border-bottom-0">
                                 <h5 class="card-title text-white">Esta es la linea de tiempo dentro de la vida del Dr.Nikola Tesla</h5>
-                                <div id="content ">
+                                <div id="content">
                                     <ul class="linea_tiempo">
-                                        <li class="event text-white-50" data-date="12:30 - 1:00pm">
-                                            <h3 class="text-white">Registration</h3>
-                                            <p>Get here on time, it's first come first serve. Be late, get turned away.</p>
+                                        <li class="event text-white-50" data-date="10 de julio de 1856">
+                                            <h3 class="text-white">Nacimiento</h3>
+                                            <p>Nikola Tesla nació, en lo que hoy es Smiljan, en la provincia serbia de Lika, llamada entonces Croacia
+                                                De una familia de 5 hermanos subestimado por su padre, un sacerdote ortodoxo serbio decidió que inventaría algo que asombraría al mundo, inicio con experimentos para aprovechar la fuerza del agua, y la fuerza del viento.
+                                            </p>
                                         </li>
-                                        <li class="event text-white-50" data-date="2:30 - 4:00pm">
-                                            <h3 class="text-white">Opening Ceremony</h3>
-                                            <p>Get ready for an exciting event, this will kick off in amazing fashion with MOP &amp; Busta Rhymes as an opening show.</p>
+                                        <li class="event text-white-50" data-date="1862">
+                                            <h3 class="text-white">Gospic</h3>
+                                            <p>El padre de Tesla, Milutin Tesla recibe un trabajo de pastor en la ciudad de Gospic a donde traslada con toda su familia y donde Tesla cursa la primaria y primeros estudios.</p>
                                         </li>
-                                        <li class="event text-white-50" data-date="5:00 - 8:00pm">
-                                            <h3 class="text-white">Main Event</h3>
-                                            <p>This is where it all goes down. You will compete head to head with your friends and rivals. Get ready!</p>
+                                        <li class="event text-white-50" data-date="1870">
+                                            <h3 class="text-white">Educación</h3>
+                                            <p>Tesla se traslada a Karlovac, donde completa sus estudios básicos en el Gimnasio de Karlovac y se gradúa un año antes debido a su desempeño académico.
+                                            </p>
                                         </li>
-                                        <li class="event text-white-50" data-date="8:30 - 9:30pm">
-                                            <h3 class="text-white">Closing Ceremony</h3>
-                                            <p>See how is the victor and who are the losers. The big stage is where the winners bask in their own glory.</p>
+                                        <li class="event text-white-50" data-date="1873">
+                                            <h3 class="text-white">Colera</h3>
+                                            <p>Tesla contrae la enfermedad del Cólera que lo dejó en cama durante apróximadamente 9 meses y en repetidas ocasiones al borde de la muerte.</p>
+                                        </li>
+                                        <li class="event text-white-50" data-date="1882">
+                                            <h3 class="text-white">Trabajo</h3>
+                                            <p>Empezó a trabajar en la Continental Edison Company en Francia, además con el sueño de trabajar con Edison en sus propios proyectos.</p>
+                                        </li>
+                                        <li class="event text-white-50" data-date="1884">
+                                            <h3 class="text-white">Estados Unidos</h3>
+                                            <p>Llegó por primera vez a Estados Unidos a la ciudad de Nueva York en donde trabajó para Edison en su compañia.</p>
+                                        </li>
+                                        <li class="event text-white-50" data-date="1885">
+                                            <h3 class="text-white">Compañia</h3>
+                                            <p>Tesla fundó su propia compañia llamada Tesla Electric Light & Manufacturing.</p>
+                                        </li>
+                                        <li class="event text-white-50" data-date="1903">
+                                            <h3 class="text-white">Wandenclyffe<h3>
+                                                    <p>Empieza a realizar experimentos en Wardenclyffe, en los cuales pretendía transmitir grandes cantidades de energía inalambricamente a toda la población de esta zona.</p>
+                                        </li>
+                                        <li class="event text-white-50" data-date="1943">
+                                            <h3 class="text-white">Muerte</h3>
+                                            <p>Muere en el hotel Wyndham en la habitación 3327 a los 86 años de edad, muere a causa de una trombosis coronaria.</p>
                                         </li>
                                     </ul>
                                 </div>
@@ -44,66 +67,17 @@ require 'views/head.php' ?>
                 </div>
             </div>
 
-            <!--Cartas-->
-            <!-- <div class="row row-cols-1 row-cols-md-5 g-4">
-                <div class="col">
-                    <div class="card">
-                        <img src="./Utilitys/tower_2.png" class="card-img-top imgcard1" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col">
-                    <div class="card ">
-                        <img src="..." class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col">
-                    <div class="card">
-                        <img src="..." class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col">
-                    <div class="card">
-                        <img src="..." class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                        </div>
-                    </div>
-                </div>
-            </div> -->
 
-            <!-- <ul class="text-white text-center">
-                <li><strong>1856</strong> - Nació en Smiljan (Gospić, Imperio austrohúngaro) Ver y modificar los
-                    datos en Wikidata
-                </li>
-            </ul>
+
+
             <blockquote class="text-white text-center" cite="https://www.goodreads.com/author/quotes/278.Nikola_Tesla">
                 <p>
                     “Si quieres encontrar los secretos del universo,
                     piensa en términos de energía, frecuencia y vibración”
                 </p>
                 <cite>― Nikola Tesla</cite>
-            </blockquote> -->
-            <!-- <h3 class="text-white m-1">
-                Si tienes tiempo, podrías leer más acerca de este increíble inventor en
-                <button type="button" class="btn btn-secondary" data-bs-toggle="tooltip" data-bs-placement="top" title="Tooltip on top">
-                    Wikipedia
-                </button> -->
-            <!-- <a id="tribute-link" class="text-white text-center text-decoration-none"
-                        href="https://es.wikipedia.org/wiki/Nikola_Tesla" target="_blank">Wikipedia
-                        entry</a>. -->
+            </blockquote>
+
             </h3>
         </section>
     </div>
