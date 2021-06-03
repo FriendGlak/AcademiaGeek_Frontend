a1d948f014d7180ea5e81ab7310f5de96df87e1f
FriendGlak <camilojheans@gmail.com>
2021-06-02 21:10:32 -0500

    Eliminados

--- views/formulario_encuesta/form_enc.php
+++
@@ -1,6 +0,0 @@
-<?php require 'views/header.php' ?>
-
-<main id="main" class="container-fluid bg-primary">
-    <img src="https://historia.nationalgeographic.com.es/medio/2019/07/09/nikola-tesla_75b66962_1200x630.jpg" alt="xd">
-</main>
-<?php require 'views/footer.php' ?>
