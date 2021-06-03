0960f599acdc1f5f99d0391a3dec89cc64d0d49f
FriendGlak <camilojheans@gmail.com>
2021-06-02 21:08:47 -0500

    Head dinámico construido

---
+++ views/head.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styles.css">
+    <!-- Bootstrap CSS -->
+    <link href="css/estilos.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+    <link rel="icon" type="image/png" href="Utilitys/favicontributepage.png" />
+    <title><?php echo $title; ?></title>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet">
+    <!-- <link rel="import" href="views/templates/head.html" id="miimport"> -->
+    <script src="js/jquery.js"></script>
+</head>
+
+<body class="bg-light">
+
