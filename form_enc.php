0ea38d631ec86b8e8558038bacffabc85f0d874a
FriendGlak <camilojheans@gmail.com>
2021-06-04 02:40:45 -0500

    Formulario agregado con validaciones

--- form_enc.php
+++ form_enc.php
@@ -1,8 +1,138 @@
-<?php $title = "Formulario Encuesta"; require 'views/head.php' ?>
+<?php $title = "Formulario Encuesta";
+require 'views/head.php' ?>
 <?php require 'views/navbar.php' ?>
-<?php require 'views/header.php' ?>
-<main id="main" class="container-fluid bg-primary">
-<p class="text-center text-light">Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum quidem quaerat accusantium natus nobis temporibus vel adipisci nostrum illum labore reiciendis tempore ullam, ut mollitia quo tempora error quasi blanditiis asperiores consectetur, accusamus officiis distinctio. Quos cumque placeat ut sequi, consectetur perspiciatis minus nam exercitationem ipsam itaque quis sed necessitatibus voluptas sapiente est cum beatae eligendi provident dolorem modi saepe molestiae veritatis, unde labore. Dolorum quibusdam consectetur eius. Maxime consequuntur molestiae distinctio quis voluptates fuga nihil. Eius, neque iure distinctio non quidem voluptas voluptatibus, molestias dicta aspernatur, odio veritatis deleniti nostrum accusamus! Veniam corrupti fugit dolorem quis aspernatur debitis, dignissimos perspiciatis aliquam error ad! Ab molestias pariatur quisquam dolores facere perspiciatis perferendis quasi deserunt beatae, saepe iusto, voluptatibus vero? Aliquam, consequuntur error quo ducimus sunt unde, id officiis omnis voluptatibus maxime repellendus similique consequatur sequi laboriosam tempora nulla sit, magni porro. Aliquam dolorum nostrum esse dolor consectetur sapiente excepturi maiores qui! Reiciendis odio ex, ratione itaque, id, aspernatur veniam nihil incidunt ipsum sunt officiis corporis tenetur asperiores molestiae dicta eos earum perspiciatis soluta unde! Animi magni fuga saepe temporibus harum cumque at deleniti. Provident nihil doloribus delectus quae corporis nobis at voluptatem est ex aut, itaque reiciendis consequatur ipsam accusamus.</p>
+
+<main id="main" class="container-fluid bg-secondary">
+    <div class="container p-5">
+        <form class="mx-auto p-4 bg-white border border-0 rounded needs-validation" style="width: 450px;" novalidate>
+            <h2 id="title" class="text-black text-center">Formulario</h2>
+
+            <!-- Nombre -->
+            <div class="form-floating mb-3">
+                <input type="name" class="form-control" id="floatingInput" placeholder="Nombres y apellidos" required>
+                <label class="text-black-50" for="floatingInput">Nombre</label>
+
+            </div>
+            <!-- Correo electrónico -->
+            <div class="form-floating mb-3">
+                <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" required>
+                <label class="text-black-50" for="floatingInput">Correo electrónico</label>
+            </div>
+            <!-- Edad -->
+            <div class="form-floating mb-3">
+                <input type="number" class="form-control" id="floatingInput" placeholder="name@example.com" required>
+                <label class="text-black-50" for="floatingInput">Edad</label>
+            </div>
+
+            <!-- Rol -->
+            <div class="mb-3 text-center">
+                <label class="text-black fw-bold" for="floatingInput">¿Qué rol cumples?</label>
+                <div>
+                    <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" required>
+                    <label class="form-check-label me-2" for="flexRadioDefault1">
+                        Maestro
+                    </label>
+                    <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" required>
+                    <label class="form-check-label me-2" for="flexRadioDefault1">
+                        Estudiante
+                    </label>
+                    <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" required>
+                    <label class="form-check-label me-2" for="flexRadioDefault1">
+                        Coordinador
+                    </label>
+                    <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" required>
+                    <label class="form-check-label me-2" for="flexRadioDefault1">
+                        Mentor
+                    </label>
+                </div>
+
+            </div>
+
+            <!-- Selección 1 -->
+            <div class="mb-3 text-center">
+                <label for="exampleInputPassword1" class="form-label text-black fw-bold">Pregunta</label>
+                <select class="form-select" aria-label="Default select example" required>
+                    <option selected disabled value>Selecciona tu respuesta</option>
+                    <option value="1">One</option>
+                    <option value="2">Two</option>
+                    <option value="3">Three</option>
+                </select>
+                <div class="invalid-feedback">
+                    Please select a valid state.
+                </div>
+            </div>
+
+            <!-- Selección 2 -->
+            <div class="mb-3 text-center">
+                <label for="exampleInputPassword1" class="form-label text-black fw-bold">Pregunta</label>
+                <select class="form-select" aria-label="Default select example" required>
+                    <option selected disabled value>Selecciona tu respuesta</option>
+                    <option value="1">One</option>
+                    <option value="2">Two</option>
+                    <option value="3">Three</option>
+                </select>
+                <div class="invalid-feedback">
+                    Please select a valid state.
+                </div>
+            </div>
+            <!-- Lenguajes programación -->
+            <div class="mb-3 form-check align-content-center">
+                <div class="text-center">
+                    <p class="text-black fw-bold">¿Qué lenguajes de programación usas?</p>
+                </div>
+                <div class="">
+                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                    <label class="form-check-label" for="exampleCheck1">Java</label>
+                </div>
+                <div>
+                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                    <label class="form-check-label" for="exampleCheck1">C#</label>
+                    <div class="invalid-feedback">
+                        You must agree before submitting.
+                    </div>
+                </div>
+                <div>
+                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                    <label class="form-check-label" for="exampleCheck1">Python</label>
+                </div>
+                <div>
+                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                    <label class="form-check-label" for="exampleCheck1">C++</label>
+                </div>
+                <div>
+                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                    <label class="form-check-label" for="exampleCheck1">C</label>
+                </div>
+                <div>
+                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                    <label class="form-check-label" for="exampleCheck1">Visual Basic .NET</label>
+                </div>
+                <div>
+                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                    <label class="form-check-label" for="exampleCheck1">JavaScript</label>
+                </div>
+                <div>
+                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                    <label class="form-check-label" for="exampleCheck1">PHP</label>
+                </div>
+                <div>
+                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                    <label class="form-check-label" for="exampleCheck1">Swift</label>
+                </div>
+                <div>
+                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                    <label class="form-check-label" for="exampleCheck1">Ninguno de los anteriores</label>
+                </div>
+            </div>
+            <div class="mx-auto" style="width: 80px;"">
+                <button type=" submit" class="btn btn-primary">Submit</button>
+            </div>
+
+        </form>
+
+
+    </div>
+
 </main>
 
 
