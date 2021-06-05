7fd317a9846d7a0c08d8715cf4d160c87d8106da
FriendGlak <camilojheans@gmail.com>
2021-06-05 03:43:33 -0500

    Navegación corregida

--- views/navbar.php
+++ views/navbar.php
@@ -1,188 +1,130 @@
-<div class="container">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light" aria-label="ejemplo">
+<div class="container wrapper">
+    <nav class="navbar navbar-expand-md navbar-light bg-light" aria-label="Navbar">
         <div class="container-fluid">
-            <div class="logo">
-                <a class="logo" href="#"><img src="utilitys/logo.png" alt="Logo"></a>
-            </div>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#ejemplo"
-                aria-controls="navbarsExample09" aria-expanded="false" aria-label="Toggle navigation">
+            <a class="navbar-brand sm align-content-center md" href="./index.php"><img src="utilitys/Logo.png" class="logo" alt="Logo Experimental" width="220"></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-
-            <div class="collapse navbar-collapse d-grid gap-2 d-md-block" id="navbarsExample09">
-                <ul class="navbar-nav navegador p-1 me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="textologo active ms-1 " aria-current="page" href="./index.php">Experimental</a>
-                    </li>
-
+            <div class="collapse navbar-collapse d-grid gap-2 d-md-block" id="navbarSupportedContent">
+                <ul class="navbar-nav p-1 me-auto mb-2 mb-lg-0">
                     <!-- Navegación -->
-                    
-                    <div class="collapse navbar-collapse ms-5" id="navbarNavLightDropdown">
-                        <ul class="navbar-nav">
+                    <div class="collapse navbar-collapse ms-5" id="navbarSupportedContent">
+                        <ul class="navbar-nav nav-links">
                             <!--Sprint 1-->
                             <li class="animnav1 nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" id="navbarLightDropdownMenuLink"
-                                    role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarLightDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                     Sprint 1
                                 </a>
-                                <ul class="udrop dropdown-menu dropdown-menu-light"
-                                    aria-labelledby="navbarLightDropdownMenuLink">
-
+                                <ul class="udrop dropdown-menu dropdown-menu-light" aria-labelledby="navbarLightDropdownMenuLink">
                                     <li class="nav-item dropdown drop-down02">
-                                        <a class="nav-link dropdown-toggle" href="#"
-                                            id="navbarLightDropdownMenuLink" role="button" data-bs-toggle="dropdown"
-                                            aria-expanded="false">
+                                        <a class="nav-link dropdown-toggle" href="#" id="navbarLightDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                             Semana 1
                                         </a>
-                                        <ul class="dropdown-menu sub-menu02 dropdown-menu-Light"
-                                            aria-labelledby="navbarLightDropdownMenuLink">
+                                        <ul class="dropdown-menu sub-menu02 dropdown-menu-Light" aria-labelledby="navbarLightDropdownMenuLink">
                                             <li><a class="dropdown-item" href="./tribute_pag.php">Página Tributo</a></li>
                                             <li><a class="dropdown-item" href="./form_enc.php">Formulario de encuesta</a></li>
                                         </ul>
                                     </li>
-
                                     <li class="nav-item dropdown drop-down02">
-                                        <a class="nav-link dropdown-toggle disabled" href="#"
-                                            id="navbarLightDropdownMenuLink" role="button" data-bs-toggle="dropdown"
-                                            aria-expanded="false">
+                                        <a class="nav-link dropdown-toggle disabled" href="#" id="navbarLightDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                             Semana 2
                                         </a>
-                                        <ul class="dropdown-menu sub-menu02 dropdown-menu-Light"
-                                            aria-labelledby="navbarLightDropdownMenuLink">
+                                        <ul class="dropdown-menu sub-menu02 dropdown-menu-Light" aria-labelledby="navbarLightDropdownMenuLink">
                                             <li><a class="dropdown-item" href="#">Producto Comercial</a></li>
-                                            <li><a class="dropdown-item" href="#">Documentación Técnica</a></li>                                              
+                                            <li><a class="dropdown-item" href="#">Documentación Técnica</a></li>
                                         </ul>
                                     </li>
-
                                     <li><a class="dropdown-item disabled" href="./adopcion.php">Aplicación</a></li>
                                 </ul>
                             </li>
-
-                             <!--Sprint 2-->
-                             <li class="nav-item dropdown ms-2 animnav1">
-                                <a class="nav-link dropdown-toggle disabled" href="#" id="navbarLightDropdownMenuLink"
-                                    role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <!--Sprint 2-->
+                            <li class="nav-item dropdown ms-2 animnav1">
+                                <a class="nav-link dropdown-toggle disabled" href="#" id="navbarLightDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                     Sprint 2
                                 </a>
-                                <ul class="udrop dropdown-menu dropdown-menu-light"
-                                    aria-labelledby="navbarLightDropdownMenuLink">
+                                <ul class="udrop dropdown-menu dropdown-menu-light" aria-labelledby="navbarLightDropdownMenuLink">
 
                                     <li class="nav-item dropdown drop-down02">
-                                        <a class="nav-link dropdown-toggle" href="#"
-                                            id="navbarLightDropdownMenuLink" role="button" data-bs-toggle="dropdown"
-                                            aria-expanded="false">
+                                        <a class="nav-link dropdown-toggle" href="#" id="navbarLightDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                             Semana 1
                                         </a>
-                                        <ul class="dropdown-menu sub-menu02 dropdown-menu-Light"
-                                            aria-labelledby="navbarLightDropdownMenuLink">
+                                        <ul class="dropdown-menu sub-menu02 dropdown-menu-Light" aria-labelledby="navbarLightDropdownMenuLink">
                                             <li><a class="dropdown-item" href="#">Página Tributo</a></li>
                                             <li><a class="dropdown-item" href="#">Formulario de encuesta</a></li>
                                         </ul>
                                     </li>
 
                                     <li class="nav-item dropdown drop-down02">
-                                        <a class="nav-link dropdown-toggle" href="#"
-                                            id="navbarLightDropdownMenuLink" role="button" data-bs-toggle="dropdown"
-                                            aria-expanded="false">
+                                        <a class="nav-link dropdown-toggle" href="#" id="navbarLightDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                             Semana 2
                                         </a>
-                                        <ul class="dropdown-menu sub-menu02 dropdown-menu-Light"
-                                            aria-labelledby="navbarLightDropdownMenuLink">
+                                        <ul class="dropdown-menu sub-menu02 dropdown-menu-Light" aria-labelledby="navbarLightDropdownMenuLink">
                                             <li><a class="dropdown-item" href="#">Producto Comercial</a></li>
-                                            <li><a class="dropdown-item" href="#">Documentación Técnica</a></li>                                              
+                                            <li><a class="dropdown-item" href="#">Documentación Técnica</a></li>
                                         </ul>
                                     </li>
 
                                     <li><a class="dropdown-item" href="#">Aplicación</a></li>
                                 </ul>
                             </li>
-
-                             <!--Sprint 3-->
-                             <li class="nav-item dropdown ms-2 animnav1">
-                                <a class="nav-link dropdown-toggle disabled" href="#" id="navbarLightDropdownMenuLink"
-                                    role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <!--Sprint 3-->
+                            <li class="nav-item dropdown ms-2 animnav1">
+                                <a class="nav-link dropdown-toggle disabled" href="#" id="navbarLightDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                     Sprint 3
                                 </a>
-                                <ul class="udrop dropdown-menu dropdown-menu-light"
-                                    aria-labelledby="navbarLightDropdownMenuLink">
+                                <ul class="udrop dropdown-menu dropdown-menu-light" aria-labelledby="navbarLightDropdownMenuLink">
 
                                     <li class="nav-item dropdown drop-down02">
-                                        <a class="nav-link dropdown-toggle" href="#"
-                                            id="navbarLightDropdownMenuLink" role="button" data-bs-toggle="dropdown"
-                                            aria-expanded="false">
+                                        <a class="nav-link dropdown-toggle" href="#" id="navbarLightDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                             Semana 1
                                         </a>
-                                        <ul class="dropdown-menu sub-menu02 dropdown-menu-Light"
-                                            aria-labelledby="navbarLightDropdownMenuLink">
+                                        <ul class="dropdown-menu sub-menu02 dropdown-menu-Light" aria-labelledby="navbarLightDropdownMenuLink">
                                             <li><a class="dropdown-item" href="#">Página Tributo</a></li>
                                             <li><a class="dropdown-item" href="#">Formulario de encuesta</a></li>
                                         </ul>
                                     </li>
-
                                     <li class="nav-item dropdown drop-down02">
-                                        <a class="nav-link dropdown-toggle" href="#"
-                                            id="navbarLightDropdownMenuLink" role="button" data-bs-toggle="dropdown"
-                                            aria-expanded="false">
+                                        <a class="nav-link dropdown-toggle" href="#" id="navbarLightDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                             Semana 2
                                         </a>
-                                        <ul class="dropdown-menu sub-menu02 dropdown-menu-Light"
-                                            aria-labelledby="navbarLightDropdownMenuLink">
+                                        <ul class="dropdown-menu sub-menu02 dropdown-menu-Light" aria-labelledby="navbarLightDropdownMenuLink">
                                             <li><a class="dropdown-item" href="#">Producto Comercial</a></li>
-                                            <li><a class="dropdown-item" href="#">Documentación Técnica</a></li>                                              
+                                            <li><a class="dropdown-item" href="#">Documentación Técnica</a></li>
                                         </ul>
                                     </li>
                                     <li><a class="dropdown-item" href="#">Aplicación</a></li>
                                 </ul>
                             </li>
-
                             <!--Sprint 4-->
                             <li class="nav-item dropdown ms-2 animnav1">
-                                <a class="nav-link dropdown-toggle disabled" href="#" id="navbarLightDropdownMenuLink"
-                                    role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <a class="nav-link dropdown-toggle disabled" href="#" id="navbarLightDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                     Sprint 4
                                 </a>
-                                <ul class="udrop dropdown-menu dropdown-menu-light"
-                                    aria-labelledby="navbarLightDropdownMenuLink">
-
+                                <ul class="udrop dropdown-menu dropdown-menu-light" aria-labelledby="navbarLightDropdownMenuLink">
                                     <li class="nav-item dropdown drop-down02">
-                                        <a class="nav-link dropdown-toggle" href="#"
-                                            id="navbarLightDropdownMenuLink" role="button" data-bs-toggle="dropdown"
-                                            aria-expanded="false">
+                                        <a class="nav-link dropdown-toggle" href="#" id="navbarLightDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                             Semana 1
                                         </a>
-                                        <ul class="dropdown-menu sub-menu02 dropdown-menu-Light"
-                                            aria-labelledby="navbarLightDropdownMenuLink">
+                                        <ul class="dropdown-menu sub-menu02 dropdown-menu-Light" aria-labelledby="navbarLightDropdownMenuLink">
                                             <li><a class="dropdown-item" href="#">Página Tributo</a></li>
                                             <li><a class="dropdown-item" href="#">Formulario de encuesta</a></li>
                                         </ul>
                                     </li>
-
                                     <li class="nav-item dropdown drop-down02">
-                                        <a class="nav-link dropdown-toggle" href="#"
-                                            id="navbarLightDropdownMenuLink" role="button" data-bs-toggle="dropdown"
-                                            aria-expanded="false">
+                                        <a class="nav-link dropdown-toggle" href="#" id="navbarLightDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                             Semana 2
                                         </a>
-                                        <ul class="dropdown-menu sub-menu02 dropdown-menu-Light"
-                                            aria-labelledby="navbarLightDropdownMenuLink">
+                                        <ul class="dropdown-menu sub-menu02 dropdown-menu-Light" aria-labelledby="navbarLightDropdownMenuLink">
                                             <li><a class="dropdown-item" href="#">Producto Comercial</a></li>
-                                            <li><a class="dropdown-item" href="#">Documentación Técnica</a></li>                                              
+                                            <li><a class="dropdown-item" href="#">Documentación Técnica</a></li>
                                         </ul>
                                     </li>
-
-                                    
-
                                     <li><a class="dropdown-item" href="#">Aplicación</a></li>
                                 </ul>
                             </li>
                         </ul>
                     </div>
-
-                    
                 </ul>
-                <form>
-                    <input class="form-control" type="text" placeholder="Search" aria-label="Search">
-                </form>
             </div>
         </div>
     </nav>
