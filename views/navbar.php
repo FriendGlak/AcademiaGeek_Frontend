9a35a1a0028f454fd49d1a0c22aeaf77638f20df
FriendGlak <camilojheans@gmail.com>
2021-06-06 06:41:26 -0500

    Ruta corregida v2

--- views/navbar.php
+++ views/navbar.php
@@ -1,7 +1,7 @@
 <div class="container wrapper">
     <nav class="navbar navbar-expand-md navbar-light bg-light" aria-label="Navbar">
         <div class="container-fluid">
-            <a class="navbar-brand sm align-content-center md" href="./index.php"><img src="utilitys/logo.png" class="logo" alt="Logo Experimental" width="220"></a>
+            <a class="navbar-brand sm align-content-center md" href="./index.php"><img src="Utilitys/logo.png" class="logo" alt="Logo Experimental" width="220"></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
