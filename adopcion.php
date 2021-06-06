40a095c576be0c87085fc544a4dabe2a22aabc43
FriendGlak <camilojheans@gmail.com>
2021-06-06 04:55:24 -0500

    Página adopción creada

--- adopcion.php
+++ adopcion.php
@@ -1,8 +1,46 @@
 <?php $title = "Adopción"; require 'views/head.php' ?>
-<?php require 'views/navbar.php' ?>
-<?php require 'views/header.php' ?>
-<main id="main" class="container-fluid bg-primary">
-<p class="text-center text-light">Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum quidem quaerat accusantium natus nobis temporibus vel adipisci nostrum illum labore reiciendis tempore ullam, ut mollitia quo tempora error quasi blanditiis asperiores consectetur, accusamus officiis distinctio. Quos cumque placeat ut sequi, consectetur perspiciatis minus nam exercitationem ipsam itaque quis sed necessitatibus voluptas sapiente est cum beatae eligendi provident dolorem modi saepe molestiae veritatis, unde labore. Dolorum quibusdam consectetur eius. Maxime consequuntur molestiae distinctio quis voluptates fuga nihil. Eius, neque iure distinctio non quidem voluptas voluptatibus, molestias dicta aspernatur, odio veritatis deleniti nostrum accusamus! Veniam corrupti fugit dolorem quis aspernatur debitis, dignissimos perspiciatis aliquam error ad! Ab molestias pariatur quisquam dolores facere perspiciatis perferendis quasi deserunt beatae, saepe iusto, voluptatibus vero? Aliquam, consequuntur error quo ducimus sunt unde, id officiis omnis voluptatibus maxime repellendus similique consequatur sequi laboriosam tempora nulla sit, magni porro. Aliquam dolorum nostrum esse dolor consectetur sapiente excepturi maiores qui! Reiciendis odio ex, ratione itaque, id, aspernatur veniam nihil incidunt ipsum sunt officiis corporis tenetur asperiores molestiae dicta eos earum perspiciatis soluta unde! Animi magni fuga saepe temporibus harum cumque at deleniti. Provident nihil doloribus delectus quae corporis nobis at voluptatem est ex aut, itaque reiciendis consequatur ipsam accusamus.</p>
-</main>
+<?php require 'views/navbar-adopcion.php' ?>
 
-<?php require 'views/footer.php' ?>
+    <div id="carouselExampleDark" class="carousel carousel-dark slide" data-bs-ride="carousel">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner">
+            <div class="carousel-item active" data-bs-interval="5000">
+                <img src="Utilitys/Slide1.png" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5></h5>
+                    <p></p>
+                </div>
+            </div>
+            <div class="carousel-item" data-bs-interval="5000">
+                <img src="Utilitys/Slide2.png" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>Encuentra tu amigo fiel</h5>
+                    <p>Cuando adoptas a una mascota, cosas maravillosas suceden en tu vida. Si estás pensando en tener un nuevo integrante en tu familia; ¡estás en el lugar correcto!</p>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="Utilitys/Slide3.png" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>Crea una nueva historia</h5>
+                    <p>Adoptar puede ser una de las experiencias más grandiosas de tu vida, donde compartirás bellos momentos con estos amiguitos fieles, tiernos y llenos de amor para regalar.</p>
+                </div>
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+
+   
+
+
+<?php require 'views/footer-adopcion.php' ?>
