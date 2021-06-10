bdd55b1a7db9be40c81a94eb26980fa51dbb2c25
FriendGlak <camilojheans@gmail.com>
2021-06-10 02:50:19 -0500

    Estructura de adopción aplicada

--- vista2-adopcion.php
+++ vista2-adopcion.php
@@ -1,22 +1,37 @@
-<?php $title = "Adopta una adorable mascota"; require 'views/head.php' ?>
+<?php $title = "Adopta una adorable mascota";
+require 'views/head.php' ?>
 <?php require 'views/navbar-adopcion.php' ?>
 <div class="container-fluid bg-rosado">
-    
-<div class="container">
-  <div class="row justify-content-md-left">
-    <div class="col col-lg-12">
-    <h4>Adopta una adorable mascota</h4>
-    </div>
-    <div class="col-md-auto">
-      Variable width content
-    </div>
-    <div class="col col-lg-12">
-      3 of 3
-    </div>
+
+  <div class="container bg-rosado">
+    <h3>Adopta una adorable mascota</h3>
+    <hr>
+    <div class="row" id="mascotas"></div>
   </div>
 
+  <template id="template-mascotas">
+    <div class="col-12 mb-2 col-md-4">
+      <div class="card cardzoom">
+        <img src="" alt="" class="img-fluid card-img-top">
+        <!--Recordar hacer imagenes de igual tamaño para un mejor manejo-->
+        <input type="hidden" id="pruebaimg" value="">
+        <div class="card-body card-img-overlay">
+          <h1 class="name-mascota text-white">Nombre</h1>
+          <h5 class=""></h5>
+          <h2 class="categoria-mascota text-white">Categoria</h2>
+          <button class="btn btn-dark">Adoptar</button>
+          <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+            Launch demo modal
+          </button>
+        </div>
+      </div>
+    </div>
+  </template>
 
-<p>Perros</p> <p>Gatos</p>
+  
+
+
+
 </div>
 
 
